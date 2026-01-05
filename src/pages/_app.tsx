0fb493f536bd562b857aece06731599cb0aa8153
EnvoyX <muhamadhanifhafizhan@gmail.com>
2026-01-05 12:56:12 +0700

    feat: setup NextAuth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,17 @@ import { HeroUIProvider } from "@heroui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -20,17 +24,19 @@ export default function App({ Component, pageProps }: AppProps) {
     },
   });
   return (
-    <QueryClientProvider client={queryClient}>
-      <HeroUIProvider>
-        <main
-          className={cn(
-            inter.className,
-            "flex min-h-screen min-w-full flex-col items-center justify-center gap-10 py-10 lg:py-0",
-          )}
-        >
-          <Component {...pageProps} />
-        </main>
-      </HeroUIProvider>
-    </QueryClientProvider>
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
+        <HeroUIProvider>
+          <main
+            className={cn(
+              inter.className,
+              "flex min-h-screen min-w-full flex-col items-center justify-center gap-10 py-10 lg:py-0",
+            )}
+          >
+            <Component {...pageProps} />
+          </main>
+        </HeroUIProvider>
+      </QueryClientProvider>
+    </SessionProvider>
   );
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,10 +1,17 @@
 import instance from "@/libs/axios/instance";
 import endpoint from "./endpoint.contants";
-import { ActivationCode, RegisterUser } from "@/types/Auth";
+import { ActivationCode, Login, RegisterUser } from "@/types/Auth";
 
 export const authServices = {
   register: (payload: RegisterUser) =>
     instance.post(`${endpoint.AUTH}/register`, payload),
   activation: (payload: ActivationCode) =>
     instance.post(`${endpoint.AUTH}/activation`, payload),
+  login: (payload: Login) => instance.post(`${endpoint.AUTH}/login`, payload),
+  getProfileWithToken: (token: string) =>
+    instance.get(`${endpoint.AUTH}/me`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }),
 };
